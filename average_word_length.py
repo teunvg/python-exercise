bcb6295ebc70039f3d984486ecffb8aadd3f5859
Teun van Gils <edu@teunvg.nl>
2021-01-15 10:41:09 +0100

    Cleaned up code.

--- average_word_length.py
+++ average_word_length.py
@@ -1,14 +1,18 @@
-# Open file
+# Script settings
 file_name = 'beyond_good_and_evil.txt'
+interpunction = ['.', ',', ':', ';', '?']
 
+# Open file
 with open(file_name) as file:
     text = file.read()
 
-# Create a list of individual word lengths
-cleaned_text = text.replace('.', '').replace(',', '') \
-                .replace(':', '').replace(';', '').replace('?', '')
+# Clean the text, remove interpunction
+for character in interpunction:
+    text = text.replace(character, '')
+
+# Create a list of individual word lengths  
 word_lengths = []
-for word in cleaned_text.split():
+for word in text.split():
     word_lengths.append(len(word))
 
 # Print the answer
