c652f6e89922dd344d320d3eb9a814400597f9a6
Teun van Gils <edu@teunvg.nl>
2021-01-15 10:22:02 +0100

    Improved solution.

--- average_word_length.py
+++ average_word_length.py
@@ -4,13 +4,10 @@ file_name = 'hamlet.txt'
 with open(file_name) as file:
     text = file.read()
 
-total_characters = 0
-word_count = 0
+# Create a list of individual word lengths
+word_lengths = []
 for word in text.split():
-    total_characters += len(word)
-    word_count += 1
+    word_lengths.append(len(word))
 
-# print(f'total_characters = {total_characters}, word_count = {word_count}')
-print(f'{total_characters=}, {word_count=}')
-
-print(f'Average word length: {total_characters/word_count}')
+# Print the answer
+print(f'Average word length: {sum(word_lengths) / len(word_lengths)}')
