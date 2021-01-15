da7469bb5ea05d72d95ac25beb3a80079ad08758
Joska de Langen <joskadelangen@gmail.com>
2021-01-15 10:40:48 +0100

    Use translation table to remove unwanted characters

--- average_word_length.py
+++ average_word_length.py
@@ -1,13 +1,29 @@
 # Open file
-file_name = 'beyond_good_and_evil.txt'
+file_name = 'hamlet.txt'
 
 with open(file_name) as file:
     text = file.read()
 
+unwanted_character_replacements = {
+    92: '', # \
+    47: '', # /
+    34: '', # "
+    39: '', # '
+    96: '', # `
+    44: '', # ,
+    46: '', # .
+    59: '', # ;
+    58: '', # :
+    40: '', # (
+    41: '', # )
+    33: '', # !
+    63: '', # ?
+}
+
 # Create a list of individual word lengths
 word_lengths = []
 for word in text.split():
-    word_lengths.append(len(word))
+    word_lengths.append(len(word.translate(unwanted_character_replacements)))
 
 # Print the answer
 print(f'Average word length: {sum(word_lengths) / len(word_lengths)}')
